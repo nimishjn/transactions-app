7898d6f9f82070010f13eb24796d4e0de7b7eda2
nimishjn <nimishjain100701@gmail.com>
2021-11-05 19:37:46 +0530

    feat: Filter and sort implemented

--- src/components/Home/CustomerTable.jsx
+++ src/components/Home/CustomerTable.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { CTable, TableTop } from "./HomeElements";
 import { FiSearch } from "react-icons/fi";
-import { VscChromeClose, VscFilter, VscFilePdf } from "react-icons/vsc";
+import { VscChromeClose, VscFilePdf } from "react-icons/vsc";
 import CustomerRow from "./CustomerRow";
 import FilterDialog from "./FilterDialog";
+import { FcEmptyFilter, FcFilledFilter } from "react-icons/fc";
 
 const CustomerTable = ({ customerData }) => {
   const [inputActive, setInputActive] = useState(false);
@@ -15,6 +16,50 @@ const CustomerTable = ({ customerData }) => {
   const customerFilter = (name) => {
     if (name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1)
       return true;
+  };
+
+  const customerFilterByOption = (element) => {
+    if (filterValue === "You will get") {
+      return element.amount > 0 && true;
+    } else if (filterValue === "You will give") {
+      return element.amount < 0 && true;
+    } else if (filterValue === "Settled") {
+      return element.amount === 0 && true;
+    } else {
+      return true;
+    }
+  };
+
+  const customerSortByOption = (element1, element2) => {
+    if (sortValue === "Highest Amount") {
+      return element2.amount - element1.amount;
+    } else if (sortValue === "By Name (A-Z)") {
+      let name1 = element1.name.toLowerCase();
+      let name2 = element2.name.toLowerCase();
+
+      if (name1 < name2) {
+        return -1;
+      }
+      if (name1 > name2) {
+        return 1;
+      }
+      return 0;
+    } else if (sortValue === "Oldest") {
+      let time1 = element1.lastTransactionDate;
+      let time2 = element2.lastTransactionDate;
+
+      if (time1 < time2) {
+        return -1;
+      }
+      if (time1 > time2) {
+        return 1;
+      }
+      return 0;
+    } else if (sortValue === "Least Amount") {
+      return Math.abs(element1.amount) - Math.abs(element2.amount);
+    } else {
+      return 0;
+    }
   };
 
   return (
@@ -47,7 +92,13 @@ const CustomerTable = ({ customerData }) => {
             />
           </div>
           <div className="customer-filter">
-            <VscFilter onClick={() => setFilterDialogOpen(true)} />
+            {filterValue === "All" && sortValue === "Most Recent" ? (
+              <FcEmptyFilter onClick={() => setFilterDialogOpen(true)} />
+            ) : (
+              <>
+                <FcFilledFilter onClick={() => setFilterDialogOpen(true)} />
+              </>
+            )}
           </div>
           <div className="customer-pdf">
             <VscFilePdf />
@@ -55,6 +106,8 @@ const CustomerTable = ({ customerData }) => {
         </TableTop>
         {customerData
           .filter((e) => customerFilter(e.name))
+          .filter((e) => customerFilterByOption(e))
+          .sort((a, b) => customerSortByOption(a, b))
           .map((e) => (
             <CustomerRow e={e} />
           ))}
